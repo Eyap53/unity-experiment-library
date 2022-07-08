30fe59e7eca5660cfd3736b0766bdc1b152ec873
Mael Lacour <public.dev@maellacour.com>
2022-07-08 11:01:31 +0200

    Prevent .meta files from being copied to Build Inputs directory.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,5 +4,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.1.0] - 2022-07-08
+# Added
+- Prevent .meta files from being copied to Build Inputs directory.
+
+
 ## [1.0.0] - 2021-10-12
 First release of the package
--- Editor/InputsFileCopy.cs
+++ Editor/InputsFileCopy.cs
@@ -1,11 +1,9 @@
 namespace ExperimentAppLibrary.Editor
 {
-	using ExperimentAppLibrary;
 	using System.IO;
-	using UnityEditor;
+	using System.Linq;
 	using UnityEditor.Build;
 	using UnityEditor.Build.Reporting;
-	using UnityEngine;
 
 #if UNITY_EDITOR_WIN || UNITY_EDITOR_LINUX
 	public class InputsFileCopy : IPostprocessBuildWithReport
@@ -18,20 +16,21 @@ namespace ExperimentAppLibrary.Editor
 			if (Directory.Exists(inputsFolder))
 			{
 				string destFolder = Path.Combine(Path.Combine(Path.GetDirectoryName(report.summary.outputPath), string.Format("{0}_Data", Path.GetFileNameWithoutExtension(report.summary.outputPath))), "Inputs");
-				DirectoryCopy(ExperimentAppLibrary.ExperimentInputs.GetInputsFolder(), destFolder, true);
+				DirectoryCopy(ExperimentAppLibrary.ExperimentInputs.GetInputsFolder(), destFolder, true, new string[] { "meta" });
 			}
 		}
 
 		/// <summary>
-		/// 
+		/// Copy a directory into the destination path. Can be recursive, and can exclude specific extensions.
 		/// </summary>
 		/// <param name="sourceDirName"></param>
 		/// <param name="destDirName"></param>
 		/// <param name="copySubDirs"></param>
+		/// <param name="extensionExceptions">Prevents files with these extensions from being copied. Do not use dots.</param>
 		/// <remarks>
 		/// Shameless copy from https://stackoverflow.com/questions/1974019/folder-copy-in-c-sharp.
 		/// </remarks>
-		private static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+		private static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs, string[] extensionExceptions = null)
 		{
 			DirectoryInfo dir = new DirectoryInfo(sourceDirName);
 			DirectoryInfo[] dirs = dir.GetDirectories();
@@ -56,17 +55,19 @@ namespace ExperimentAppLibrary.Editor
 
 			foreach (FileInfo file in files)
 			{
-				// Create the path to the new copy of the file.
-				string temppath = Path.Combine(destDirName, file.Name);
+				if (extensionExceptions == null || !extensionExceptions.Contains(file.Extension))
+				{
+					// Create the path to the new copy of the file.
+					string temppath = Path.Combine(destDirName, file.Name);
 
-				// Copy the file.
-				file.CopyTo(temppath, true); // OVERWRITE IF EXIST ALREADY
+					// Copy the file.
+					file.CopyTo(temppath, true); // OVERWRITE IF EXIST ALREADY
+				}
 			}
 
 			// If copySubDirs is true, copy the subdirectories.
 			if (copySubDirs)
 			{
-
 				foreach (DirectoryInfo subdir in dirs)
 				{
 					// Create the subdirectory.
