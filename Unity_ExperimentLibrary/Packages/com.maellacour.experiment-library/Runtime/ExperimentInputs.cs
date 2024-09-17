04b1f469c3b86fc84315636f4d5d3a5fe92d9d2d
Mael Lacour <public.dev@maellacour.com>
2024-09-17 14:46:18 +0200

    docs: write docs for ExperimentInputs

--- Unity_ExperimentLibrary/Packages/com.maellacour.experiment-library/Runtime/ExperimentInputs.cs
+++ Unity_ExperimentLibrary/Packages/com.maellacour.experiment-library/Runtime/ExperimentInputs.cs
@@ -9,19 +9,49 @@ namespace ExperimentLibrary
 	using System.IO;
 	using UnityEngine;
 
+	/// <summary>
+	/// Provides methods to handle experiment input files, including reading participant-specific and common input files.
+	/// </summary>
 	public class ExperimentInputs
 	{
+		/// <summary>
+		/// Gets the path to the inputs folder.
+		/// </summary>
+		/// <returns>The path to the inputs folder.</returns>
 		public static string GetInputsFolder()
 		{
 			return Path.Combine(@"" + Application.dataPath, "Inputs");
 		}
 
+		/// <summary>
+		/// Gets the path to the folder for a specific participant.
+		/// </summary>
+		/// <param name="participantId">The ID of the participant.</param>
+		/// <returns>The path to the participant's folder.</returns>
 		public static string GetParticipantFolder(int participantId)
 		{
 			return Path.Combine(GetInputsFolder(), participantId.ToString());
 		}
 
+		/// <summary>
+		/// Reads the input file for a specific participant and maps it to an array of objects of type <typeparamref name="T"/>.
+		/// </summary>
+		/// <typeparam name="T">The type of objects to map the CSV data to.</typeparam>
+		/// <typeparam name="UMap">The class map type for CSV mapping.</typeparam>
+		/// <param name="participantId">The ID of the participant.</param>
+		/// <param name="fileName">The name of the input file.</param>
+		/// <returns>An array of objects of type <typeparamref name="T"/>.</returns>
 		public static T[] ReadParticipantInput<T, UMap>(int participantId, string fileName) where UMap : ClassMap => ReadParticipantInput<T>(participantId, fileName, ObjectResolver.Current.Resolve<UMap>());
+
+		/// <summary>
+		/// Reads the input file for a specific participant and maps it to an array of objects of type <typeparamref name="T"/>.
+		/// </summary>
+		/// <typeparam name="T">The type of objects to map the CSV data to.</typeparam>
+		/// <param name="participantId">The ID of the participant.</param>
+		/// <param name="fileName">The name of the input file.</param>
+		/// <param name="map">The class map for CSV mapping. If null, default mapping is used.</param>
+		/// <returns>An array of objects of type <typeparamref name="T"/>.</returns>
+		/// <exception cref="ArgumentException">Thrown when <paramref name="fileName"/> is null or whitespace.</exception>
 		public static T[] ReadParticipantInput<T>(int participantId, string fileName, ClassMap map = null)
 		{
 			if (string.IsNullOrWhiteSpace(fileName))
@@ -34,7 +64,23 @@ namespace ExperimentLibrary
 			return ReadCsvInput<T>(filePath, map);
 		}
 
+		/// <summary>
+		/// Reads a common input file and maps it to an array of objects of type <typeparamref name="T"/>.
+		/// </summary>
+		/// <typeparam name="T">The type of objects to map the CSV data to.</typeparam>
+		/// <typeparam name="UMap">The class map type for CSV mapping.</typeparam>
+		/// <param name="fileName">The name of the input file.</param>
+		/// <returns>An array of objects of type <typeparamref name="T"/>.</returns>
 		public static T[] ReadCommonInput<T, UMap>(string fileName) where UMap : ClassMap => ReadCommonInput<T>(fileName, ObjectResolver.Current.Resolve<UMap>());
+
+		/// <summary>
+		/// Reads a common input file and maps it to an array of objects of type <typeparamref name="T"/>.
+		/// </summary>
+		/// <typeparam name="T">The type of objects to map the CSV data to.</typeparam>
+		/// <param name="fileName">The name of the input file.</param>
+		/// <param name="map">The class map for CSV mapping. If null, default mapping is used.</param>
+		/// <returns>An array of objects of type <typeparamref name="T"/>.</returns>
+		/// <exception cref="ArgumentException">Thrown when <paramref name="fileName"/> is null or whitespace.</exception>
 		public static T[] ReadCommonInput<T>(string fileName, ClassMap map = null)
 		{
 			if (string.IsNullOrWhiteSpace(fileName))
@@ -46,7 +92,24 @@ namespace ExperimentLibrary
 			return ReadCsvInput<T>(filePath, map);
 		}
 
+		/// <summary>
+		/// Reads a CSV input file and maps it to an array of objects of type <typeparamref name="T"/>.
+		/// </summary>
+		/// <typeparam name="T">The type of objects to map the CSV data to.</typeparam>
+		/// <typeparam name="UMap">The class map type for CSV mapping.</typeparam>
+		/// <param name="filePath">The path to the CSV file.</param>
+		/// <returns>An array of objects of type <typeparamref name="T"/>.</returns>
+		/// <exception cref="ArgumentException">Thrown when <paramref name="filePath"/> is null, whitespace, or the file does not exist.</exception>
 		public static T[] ReadCsvInput<T, UMap>(string filePath) where UMap : ClassMap => ReadCsvInput<T>(filePath, ObjectResolver.Current.Resolve<UMap>());
+
+		/// <summary>
+		/// Reads a CSV input file and maps it to an array of objects of type <typeparamref name="T"/>.
+		/// </summary>
+		/// <typeparam name="T">The type of objects to map the CSV data to.</typeparam>
+		/// <param name="filePath">The path to the CSV file.</param>
+		/// <param name="map">The class map for CSV mapping. If null, default mapping is used.</param>
+		/// <returns>An array of objects of type <typeparamref name="T"/>.</returns>
+		/// <exception cref="ArgumentException">Thrown when <paramref name="filePath"/> is null, whitespace, or the file does not exist.</exception>
 		public static T[] ReadCsvInput<T>(string filePath, ClassMap map = null)
 		{
 			if (string.IsNullOrWhiteSpace(filePath))
@@ -71,6 +134,14 @@ namespace ExperimentLibrary
 			return result;
 		}
 
+		/// <summary>
+		/// Reads the settings file for a specific participant and maps it to an object of type <typeparamref name="TSettings"/>.
+		/// </summary>
+		/// <typeparam name="TSettings">The type of the settings object.</typeparam>
+		/// <param name="participantId">The ID of the participant.</param>
+		/// <param name="fileName">The name of the settings file. Defaults to "Settings".</param>
+		/// <returns>An object of type <typeparamref name="TSettings"/> if the file exists; otherwise, the default value of <typeparamref name="TSettings"/>.</returns>
+		/// <exception cref="ArgumentException">Thrown when <paramref name="fileName"/> is null or whitespace.</exception>
 		public static TSettings ReadParticipantSettings<TSettings>(int participantId, string fileName = "Settings")
 		{
 			if (string.IsNullOrWhiteSpace(fileName))
