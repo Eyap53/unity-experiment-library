80ea6c432a663f22c4cff07768b2f1c7bc527462
Mael Lacour <public.dev@maellacour.com>
2024-09-17 14:42:28 +0200

    feat: Do not copy inputs directory if empty

--- Unity_ExperimentLibrary/Packages/com.maellacour.experiment-library/Editor/InputsFileCopy.cs
+++ Unity_ExperimentLibrary/Packages/com.maellacour.experiment-library/Editor/InputsFileCopy.cs
@@ -5,17 +5,43 @@ namespace ExperimentLibrary.Editor
 	using UnityEditor.Build.Reporting;
 
 #if UNITY_EDITOR_WIN || UNITY_EDITOR_LINUX
+	/// <summary>
+	/// Handles the copying of input files to the build output directory after the build process.
+	/// </summary>
 	public class InputsFileCopy : IPostprocessBuildWithReport
 	{
+		/// <summary>
+		/// Gets the callback order for the post-process build step.
+		/// </summary>
 		public int callbackOrder { get { return 0; } }
 
+		/// <summary>
+		/// Called after the build process is complete. Copies the input files to the build output directory.
+		/// </summary>
+		/// <param name="report">The report containing information about the build.</param>
 		public void OnPostprocessBuild(BuildReport report)
 		{
-			string inputsFolder = ExperimentLibrary.ExperimentInputs.GetInputsFolder();
-			if (Directory.Exists(inputsFolder))
+			// Get the folder containing the input files.
+			string inputsFolder = ExperimentInputs.GetInputsFolder();
+
+			// Only proceed if the input files folder exists and is not empty
+			if (Directory.Exists(inputsFolder) && Directory.GetFileSystemEntries(inputsFolder).Length > 0)
 			{
-				string destFolder = Path.Combine(Path.Combine(Path.GetDirectoryName(report.summary.outputPath), string.Format("{0}_Data", Path.GetFileNameWithoutExtension(report.summary.outputPath))), "Inputs");
-				DirectoryExtensions.Copy(ExperimentLibrary.ExperimentInputs.GetInputsFolder(), destFolder, true, new string[] { "meta" });
+				// Determine the destination folder for the input files.
+				string folderName = $"{Path.GetFileNameWithoutExtension(report.summary.outputPath)}_Data";
+				string destFolder = Path.Combine(Path.Combine(Path.GetDirectoryName(report.summary.outputPath), folderName), "Inputs");
+
+				// Copy the input files to the destination folder, excluding files with the "meta" extension.
+				DirectoryExtensions.Copy(
+								sourceDirName: ExperimentInputs.GetInputsFolder(),
+								destDirName: destFolder,
+								copySubDirs: true,
+								extensionExceptions: new string[] { "meta" }
+				);
+			}
+			else
+			{
+				UnityEngine.Debug.Log($"ExperimentLibrary: No input files found in {inputsFolder}. Skipping copy operation.");
 			}
 		}
 	}
